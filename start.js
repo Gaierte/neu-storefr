6ace94d9440d3fc0a0f565a2845aba764e4a3724
Gaierte <2312713248@qq.com>
2020-08-17 18:15:23 +0800

    feat：都删了

--- start.js
+++
@@ -1,9 +0,0 @@
-console.log("我在master里面");
-console.log("master");
-console.log("poipoi");
-console.log("master的东西变成hck1的了");
-console.log("hck1修改后变成了hck的");
-console.log("我应该要被忽略的啊！！！);
-console.log(这次应该别忽略了吧！)；
-ole.log();
-
