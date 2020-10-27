6df3f02385cbe846fcf05a17a111f68d9563c940
Gaierte <2312713248@qq.com>
2020-10-27 19:35:33 +0800

    feat:初始化了reducers.js

---
+++ src/redux/reduers.js
@@ -0,0 +1,11 @@
+import {combineReducers} from 'redux'
+
+function xxx(prevState,action){
+  switch(action.type){
+    case:xxx;return xx;
+    default:prevState;
+  }
+}
+export default combineReducers({
+  xxx,
+});
